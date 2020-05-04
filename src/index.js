8c1d0fce7d732d3d394a2dbb7d3b7b5a1c6c5f90
Daniil <daniilterawars@gmail.com>
2020-05-04 18:53:01 +0300

    create layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import React from 'react'
+import {applyMiddleware, createStore} from "redux";
+import {createBrowserHistory} from "history";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {Provider} from "react-redux";
+
+import createRootReducer from 'reducers'
+import routes from 'routes'
+
+const history = createBrowserHistory()
+const middlewares  = [thunk, routerMiddleware(history)]
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Provider store = {store}>
+        <ConnectedRouter history = {history}>
+            {routes}
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
